6a33934b4d99b7f6849760a7abcfe6f3df97304e
Gabriela Pinheiro <gabipinheiro38@gmail.com>
2023-03-27 20:52:00 -0300

    fix(components): update result page components

--- components/Pill/Pill.styles.ts
+++ components/Pill/Pill.styles.ts
@@ -2,10 +2,10 @@ import { Chip } from '@mui/material'
 import { styled } from '@mui/system'
 
 export const Pill = styled(Chip)({
-	fontSize: "24px",
-	fontWeight: "bold",
-	height: "50px",
-	borderRadius: "32px",
-  paddingLeft: "10px",
-  paddingRight: "10px",
-});
+  fontSize: '24px',
+  fontWeight: 'bold',
+  height: '50px',
+  borderRadius: '32px',
+  paddingLeft: '10px',
+  paddingRight: '10px'
+})
--- pages/result.tsx
+++ pages/result.tsx
@@ -1,4 +1,4 @@
-import { Pill } from "@/components/Pill/Pill.styles"
+import { Pill } from '@/components/Pill/Pill.styles'
 import { Stack } from '@mui/system'
 import { Title, Subtitle } from '../components/Typography/Typography.styles'
 
@@ -6,12 +6,10 @@ export default function Result() {
   return (
     <Stack spacing={2} alignItems="center">
       <Title variant="h1">Tabela Fipe: Preco chevrolet cruze 2019</Title>
-      <Pill
-        variant="filled"
-        color="secondary"
-        label="R$ 987.000,00"
-      />
-      <Subtitle variant="caption" sx={{ fontSize: "15px" }}>Este é o preço de compra do veículo</Subtitle>
+      <Pill variant="filled" color="secondary" label="R$ 987.000,00" />
+      <Subtitle variant="caption" sx={{ fontSize: '15px' }}>
+        Este é o preço de compra do veículo
+      </Subtitle>
     </Stack>
   )
 }
